ad53d10fd101b97ac224b89ca86b99f2e642f7fd
Alexandr Morozov // chillipepper <newsskyx@gmail.com>
2022-02-08 00:37:48 +0200

    set 2.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulGallery = document.querySelector('.gallery');
+
+// 1
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a href="${original}"><img src="${preview}" alt="${description}" title="/* ${description} */"  width="100%" height="99%"/></a>`;
+  })
+  .join('');
+
+ulGallery.insertAdjacentHTML('afterbegin', markup);
+
+//// 2
+ulGallery.addEventListener('click', openModal);
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.tagName !== 'IMG') {
+    return;
+  }
+
+  const selectedImg = event.target.src;
+  console.log('1: ', selectedImg);
+
+  const modalFrame = new SimpleLightbox('.gallery a', {
+    /* options */
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+}
